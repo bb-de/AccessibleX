e978b5a63d0206c98726e6d3be5eaa4feb2ef0d8
bb-de <113197046+bb-de@users.noreply.github.com>
2025-05-26 14:57:35 +0200

    Create WidgetIntegrationPage.tsx

---
+++ client/src/pages/WidgetIntegrationPage.tsx
@@ -0,0 +1,8 @@
+export default function WidgetIntegrationPage() {
+  return (
+    <div>
+      <h2>Widget Integration</h2>
+      <p>Hier kommt deine Integration oder Testumgebung hin.</p>
+    </div>
+  );
+}
